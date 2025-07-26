a13e69e6bb45304f368cf6f8bab7e1a47f7e3e91
Ikram Akram <ik726akram@gmail.com>
2025-07-26 16:19:33 +0500

    add authorization in headers

--- src/api.js
+++ src/api.js
@@ -1,11 +1,14 @@
 // api.js
 import axios from 'axios';
 
+const basicAuth = btoa("user:mysecurepass123"); // base64 encoding
+
 export default axios.create({
-  baseURL: 'https://user:mysecurepass123@intimate-wildly-mammal.ngrok-free.app/api/v1',
+  baseURL: "https://intimate-wildly-mammal.ngrok-free.app/api/v1",
   headers: {
-    'X-API-KEY': process.env.REACT_APP_API_KEY || '8db7b26a4c4f479a9cba9465d9ec8d7e',
-    'Content-Type': 'application/json',
+    "Authorization": `Basic ${basicAuth}`,
+    "X-API-KEY": process.env.REACT_APP_API_KEY || "8db7b26a4c4f479a9cba9465d9ec8d7e",
+    "Content-Type": "application/json",
   },
 });
 
